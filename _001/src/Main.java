bccb72528b1dda0474a99d8599885e1c6661ac72
Kil3ma <amelia.lenarci@gmail.com>
2024-02-21 19:20:14 +0100

    moj pierwszy commit

--- _001/src/Main.java
+++ _001/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
+        var firstNumbers = getFirstNumbers(99999);
         System.out.println("Hello world!");
     }
+
+    private static List<Integer> getFirstNumbers(int toNumber) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        //kod
+        return primeNumbers;
+    }
+
+    private static List<Integer> getPrimeFactors(int number, List<Integer> primeFactors) {
+
+        return primeFactors;
+    }
+
+    private static boolean isOurNumber(List<Integer> primeFactors) {
+
+        return false;
+    }
+
 }
